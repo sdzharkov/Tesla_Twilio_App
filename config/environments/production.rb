04107660a035f86965ec32d9a6c2ad8a3600484b
sdzharkov <stephan.zharkov@gmail.com>
2017-12-18 22:36:46 -0800

    Add necessary Heroku Info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.config.web_socket_server_url = "wss://tesla-twilio-app.herokuapp.com/cable"
+  config.config.action_cable.allowed_request_origins = ['https://tesla-twilio-app.herokuapp.com', 'https://tesla-twilio-frontend.herokuapp.com/']
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
