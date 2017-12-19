fd42fa3870ecdc416408d5f56a5dfd0eae7dd396
sdzharkov <stephan.zharkov@gmail.com>
2017-12-18 23:01:20 -0800

    update config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,9 +32,11 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.mount_path = nil
+  config.action_cable.url = "wss://tesla-twilio-app.herokuapp.com/cable"
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  # config.web_socket_server_url = "wss://tesla-twilio-app.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://tesla-twilio-app.herokuapp.com', /https:\/\/tesla-twilio-app.herokuapp.*/, 'https://tesla-twilio-frontend.herokuapp.com/', /https:\/\/tesla-twilio-frontend.herokuapp.*/]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -67,9 +69,6 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
-  config.web_socket_server_url = "wss://tesla-twilio-app.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://tesla-twilio-app.herokuapp.com', 'https://tesla-twilio-frontend.herokuapp.com/']
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
